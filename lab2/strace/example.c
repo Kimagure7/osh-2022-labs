91b81f2af74f41761b2036c66d5e2e287ccd683f
Kimagure <re2477036@mail.ustc.edu.cn>
2022-05-08 19:26:33 +0800

    fatal

--- lab2/strace/example.c
+++ lab2/strace/example.c
@@ -16,36 +16,38 @@
 #include <syscall.h>
 #include <sys/ptrace.h>
 
-#define FATAL(...) \
-    do { \
+#define FATAL(...)                               \
+    do                                           \
+    {                                            \
         fprintf(stderr, "strace: " __VA_ARGS__); \
-        fputc('\n', stderr); \
-        exit(EXIT_FAILURE); \
+        fputc('\n', stderr);                     \
+        exit(EXIT_FAILURE);                      \
     } while (0)
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     if (argc <= 1)
         FATAL("too few arguments: %d", argc);
 
     pid_t pid = fork();
-    switch (pid) {
-        case -1: /* error */
-            FATAL("%s", strerror(errno));
-        case 0:  /* child */
-            ptrace(PTRACE_TRACEME, 0, 0, 0);
-            /* Because we're now a tracee, execvp will block until the parent
-             * attaches and allows us to continue. */
-            execvp(argv[1], argv + 1);
-            FATAL("%s", strerror(errno));
+    switch (pid)
+    {
+    case -1: /* error */
+        FATAL("%s", strerror(errno));
+    case 0: /* child */
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        /* Because we're now a tracee, execvp will block until the parent
+         * attaches and allows us to continue. */
+        // execvp(argv[1], argv + 1);
+        FATAL("%s", strerror(errno));
     }
 
     /* parent */
     waitpid(pid, 0, 0); // sync with execvp
     ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_EXITKILL);
 
-    for (;;) {
+    for (;;)
+    {
         /* Enter next system call */
         if (ptrace(PTRACE_SYSCALL, pid, 0, 0) == -1)
             FATAL("%s", strerror(errno));
@@ -62,7 +64,7 @@ main(int argc, char **argv)
         fprintf(stderr, "%ld(%ld, %ld, %ld, %ld, %ld, %ld)",
                 syscall,
                 (long)regs.rdi, (long)regs.rsi, (long)regs.rdx,
-                (long)regs.r10, (long)regs.r8,  (long)regs.r9);
+                (long)regs.r10, (long)regs.r8, (long)regs.r9);
 
         /* Run system call and stop on exit */
         if (ptrace(PTRACE_SYSCALL, pid, 0, 0) == -1)
@@ -71,7 +73,8 @@ main(int argc, char **argv)
             FATAL("%s", strerror(errno));
 
         /* Get system call result */
-        if (ptrace(PTRACE_GETREGS, pid, 0, &regs) == -1) {
+        if (ptrace(PTRACE_GETREGS, pid, 0, &regs) == -1)
+        {
             fputs(" = ?\n", stderr);
             if (errno == ESRCH)
                 exit(regs.rdi); // system call was _exit(2) or similar
--- lab2/strace/strace.cpp
+++ lab2/strace/strace.cpp
@@ -1,8 +1,34 @@
-#include<iostream>
-#include<sys/ptrace.h>
+#include <iostream>
+#define _POSIX_C_SOURCE 200112L
+/* C standard library */
+#include <errno.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* POSIX */
+#include <unistd.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+
+/* Linux */
+#include <syscall.h>
+#include <sys/ptrace.h>
 
 using namespace std;
 
-int main(){
-    
+//使用实例程序中的报错函数 实现看不是很懂 但是确实好用
+//...与va args似乎表示可变参数 所以传递的时候会是那种形式
+#define FATAL(...)                               \
+    do                                           \
+    {                                            \
+        fprintf(stderr, "strace: " __VA_ARGS__); \
+        fputc('\n', stderr);                     \
+        exit(EXIT_FAILURE);                      \
+    } while (0)
+
+int main()
+{
+    pid_t pid = fork();
 }
