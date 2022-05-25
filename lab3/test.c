8c909d115aacb05fa4699f86c704584d10548d83
Kimagure <re2477036@mail.ustc.edu.cn>
2022-05-25 21:28:24 +0800

    学习ing

---
+++ lab3/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+int main(){
+    printf("%hd",htons(6666));
+}
