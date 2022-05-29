a00903f82c69c50aa6a11bbb7aab85b10d318f35
Kimagure <re2477036@mail.ustc.edu.cn>
2022-05-29 20:42:44 +0800

    0

--- lab3/2ptest.cpp
+++ lab3/2ptest.cpp
@@ -1,82 +1,177 @@
-#pragma once
- 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include <queue>
-#include <memory>
- 
- 
-template <typename T>
-class threadsafe_queue 
+
+#define BUF 1024
+#define MAX_USERS 32
+using namespace std;
+struct USER
 {
-private:
-	struct node {
-		std::shared_ptr<T> data;
- 
-		//为什么要用unique_ptr,如果想做细粒度的锁控制,那么unique_ptr是必须的,细粒度控制意味着局部加锁,且各个动作之前应当是互不干扰的
-		//比如pop只操作队列头部,push只操作队列尾部,这样的话就可以保证生产者线程(调用push的线程)和消费者线程(调用pop的线程)可以完全并发
-		//进行。但是多个生产者之间还是需要同步,多个生产者之间也需要互相同步,且同步使用的就是局部锁。
-		//unique_ptr可以保证指针不会被导出,这样的话外部就无法对指针额外加锁和销毁指针。
-		//局部锁是并发访问的理论基础。
-		//并发访问的核心思想就是通过为固有/常见的数据结构添加一些辅助节点,以实现生产者和消费者的访问隔离,在此基础上,如果有多个生产者和多个
-		//消费者,则只提供生产者同步锁和消费者同步锁即可。
-		std::unique_ptr<node> next;			
-	};
- 
-	std::mutex head_m;
-	std::mutex tail_m;
-	std::unique_ptr<node> head;
-	node* tail;
- 
-	node* get_tail() 
-	{
-		std::lock_guard<std::mutex> lock_tail(tail_m);
-		return tail;
-	}
- 
-	std::unique_ptr<node> pop_head()
-	{
-		std::lock_guard<std::mutex> lock_head(head_m);
- 
-		if (head.get() == get_tail()) {
-			return nullptr;
-		}
- 
-		std::unique_ptr<node> old_head = std::move(head);
-		head = std::move(old_head->next);
-		return old_head;
-	}
- 
-public:
-	threadsafe_queue()
-		:head(new node)
-		, tail(head.get())
-	{}
- 
-	threadsafe_queue(const threadsafe_queue& other) = delete;
-	threadsafe_queue& operator=(const threadsafe_queue& other) = delete;
- 
-	std::shared_ptr<T> try_pop() 
-	{
-		std::unique_ptr<node> old_head = pop_head();
-		return old_head ? old_head->data : std::shared_ptr<T>();
-	}
- 
-	void push(T new_value) 
-	{
-		std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
-		std::unique_ptr<node> p(new node);
-		node* const new_tail = p.get();
-		std::lock_guard<std::mutex> tail_lock(tail_m);
-		tail->data = new_data;
-		tail->next = std::move(p);
-		tail = new_tail;
-	}
+    //向所有用户发送信息 接受的时候也是全部接收 不需要使用fd_send了
+    // fd_recv表示从哪里接受数据 是终端
+    int fd_recv;
+    int id;
 };
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+int client[MAX_USERS];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int used[MAX_USERS] = {0}; //用户池
+
+//实现思路：
+//每个用户享有一个消息队列
+//hand_chat中实现对队列的push
+//主程序中实现send
+
+void *handle_chat(void *data)
+{
+    struct USER *user = (struct USER *)data;
+    char buffer[BUF * 2];
+    char buffer_mess[BUF];
+    ssize_t len;
+	queue<char*> mesg;
+    sprintf(buffer, "user %2d: ", user->id);
+    while (1)
+    {
+        len = recv(user->fd_recv, buffer_mess, BUF - 24, 0);
+        if (len <= 0)
+        {
+            // <0 : error   =0: 连接中断
+            used[user->id] = 0;
+            close(client[user->id]);
+            return 0;
+        }
+
+        int temp = 0; //记录上一个\n位置
+        // puts(buffer_mess);
+        // fflush(stdout);
+        for (int i = 0; i < len; i++)
+        {
+            if (buffer_mess[i] == '\n')
+            {
+                strncpy(buffer + 9, buffer_mess + temp, i + 1 - temp);
+                // 第二步处理 如果send没发全 就while一直发出去
+                int length = strlen(buffer);
+                int send_length = 0; //记录已发送的字节数
+                int send_return = 0; // send的返回值
+                pthread_mutex_lock(&mutex);
+                for (int j = 0; j < MAX_USERS; j++)
+                {
+                    if (used[j] && j != user->id)
+                    {
+                        while (send_length < length)
+                        {
+                            send_return = send(client[j], buffer + send_length, length - send_length, 0); //每次都尝试发送剩下的所有信息
+                            if (send_return == -1)
+                            {
+                                perror("send");
+                                exit(-1);
+                            }
+                            send_length += send_return;
+                        }
+                        send_return = 0;
+                        send_length = 0;
+                    }
+                }
+                pthread_mutex_unlock(&mutex);
+                memset(buffer, 0, sizeof(buffer));
+                sprintf(buffer, "user %2d: ", user->id);//恢复初始状态
+                temp = i + 1;                //+1 是为了跳过换行符
+            }
+        }
+        if (temp != strlen(buffer_mess)) //!= len也可以
+        {
+            //如果沒有換行符
+            strncpy(buffer + 9, buffer_mess, strlen(buffer_mess)); //全弄过来
+            int length = strlen(buffer);
+            int send_length = 0; //记录已发送的字节数
+            int send_return = 0; // send的返回值
+            pthread_mutex_lock(&mutex);
+            for (int j = 0; j < MAX_USERS; j++)
+            {
+                if (used[j] && j != user->id)
+                {
+                    while (send_length < length)
+                    {
+                        send_return = send(client[j], buffer + send_length, length - send_length, 0); //每次都尝试发送剩下的所有信息
+                        if (send_return == -1)
+                        {
+                            perror("send");
+                            exit(-1);
+                        }
+                        send_length += send_return;
+                    }
+                    send_return = 0;
+                    send_length = 0;
+                }
+            }
+            pthread_mutex_unlock(&mutex);
+            memset(buffer, 0, sizeof(buffer));
+            sprintf(buffer, "user %2d: ", user->id); //恢复初始状态
+        }
+        memset(buffer_mess, 0, sizeof(buffer_mess));
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int port = atoi(argv[1]);
+    int fd;
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket");
+        return 1;
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    socklen_t addr_len = sizeof(addr);
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)))
+    {
+        perror("bind");
+        return 1;
+    }
+    if (listen(fd, MAX_USERS))
+    {
+        perror("listen");
+        return 1;
+    }
+
+    pthread_t thread[MAX_USERS];
+    struct USER user[MAX_USERS];
+
+    while (1)
+    {
+        int i;
+        int temp_client = accept(fd, NULL, NULL);
+        if (temp_client == -1)
+        {
+            perror("accept");
+            return 1;
+        }
+
+        for (i = 0; i < MAX_USERS; i++)
+        {
+            if (used[i] == 0)
+            { //找一个没使用的id分配
+                used[i] = 1;
+                client[i] = temp_client;
+                user[i].fd_recv = temp_client;
+                user[i].id = i;
+                pthread_create(&thread[i], NULL, handle_chat, (void *)&user[i]);
+                break;
+            }
+        }
+        if (i == MAX_USERS)
+        {
+            perror("MAX_USERS");
+            return 1;
+        }
+    }
+    return 0;
+}
