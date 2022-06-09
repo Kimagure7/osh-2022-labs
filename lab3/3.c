72c63437509260253e608d7eb9d56521f358a404
Kimagure <re2477036@mail.ustc.edu.cn>
2022-06-09 23:17:12 +0800

    bug

--- lab3/3.c
+++ lab3/3.c
@@ -19,72 +19,35 @@ void handle_chat(int id)
     char buffer[BUF * 2];
     char buffer_mess[BUF];
     ssize_t len;
-    int first_recv = 1; //必须使用while 1 但是只能处理一次msg
     sprintf(buffer, "user %2d: ", id);
 
-    while (1)
+    len = recv(client[id], buffer_mess, BUF - 24, 0); //需要注意这里已经是非阻塞的了
+    if (len <= 0)
     {
-        len = recv(client[id], buffer_mess, BUF - 24, 0);//需要注意这里已经是非阻塞的了
-        if (len <= 0)
-        {
-            if (first_recv)
-            {
-                // <0 : error   =0: 连接中断
-                used[id] = 0;
-                close(client[id]);//这里似乎很关键 推测多次close会出现问题
-                return;
-            }
-            return;
-        }
-        first_recv = 0;
+        // <0 : error   =0: 连接中断
+        used[id] = 0;
+        close(client[id]); //处理完一条信息才返回
+        return;
+    }
 
-        int temp = 0; //记录上一个\n位置
-        // puts(buffer_mess);
-        // fflush(stdout);
-        for (int i = 0; i < len; i++)
+    int temp = -1; //记录上一个\n位置
+    // puts(buffer_mess);
+    // fflush(stdout);
+    for (int i = 0; i < len; i++)
+    {
+        // TODO：太长了而没有换行符
+        if (buffer_mess[i] == '\n')
         {
-            // TODO：太长了而没有换行符
-            if (buffer_mess[i] == '\n')
-            {
-                strncpy(buffer + 9, buffer_mess + temp, i + 1 - temp);
-                // TODO:第二步处理 如果send没发全 就while一直发出去
-                int length = strlen(buffer);
-                int send_length = 0; //记录已发送的字节数
-                int send_return = 0; // send的返回值
-                for (int j = 0; j < MAX_USERS; j++)
-                {
-                    if (used[j] && j != id)
-                    {
-                        while (send_length < length)
-                        {
-                            send_return = send(client[j], buffer + send_length, length - send_length, 0); //每次都尝试发送剩下的所有信息
-                            if (send_return == -1)
-                            {
-                                perror("send");
-                                exit(-1);
-                            }
-                            send_length += send_return;
-                        }
-                        send_return = 0;
-                        send_length = 0;
-                    }
-                }
-                memset(buffer, 0, sizeof(buffer));
-                sprintf(buffer, "user %2d: ", id); //恢复初始状态
-                temp = i + 1;                      //+1 是为了跳过换行符
-            }
-        }
-        if (temp != strlen(buffer_mess)) //!= len也可以
-        {
-            //如果沒有換行符
-            strncpy(buffer + 9, buffer_mess, strlen(buffer_mess)); //全弄过来
-            int length = strlen(buffer);
-            int send_length = 0; //记录已发送的字节数
-            int send_return = 0; // send的返回值
+            strncpy(buffer + 9, buffer_mess + temp + 1, i - temp);
+            // TODO:第二步处理 如果send没发全 就while一直发出去
+            int length = i - temp + 9;
+
             for (int j = 0; j < MAX_USERS; j++)
             {
                 if (used[j] && j != id)
                 {
+                    int send_length = 0; //记录已发送的字节数
+                    int send_return = 0; // send的返回值
                     while (send_length < length)
                     {
                         send_return = send(client[j], buffer + send_length, length - send_length, 0); //每次都尝试发送剩下的所有信息
@@ -95,14 +58,35 @@ void handle_chat(int id)
                         }
                         send_length += send_return;
                     }
-                    send_return = 0;
-                    send_length = 0;
                 }
             }
-            memset(buffer, 0, sizeof(buffer));
-            sprintf(buffer, "user %2d: ", id); //恢复初始状态
+            temp = i;                      //+1 是为了跳过换行符
         }
-        memset(buffer_mess, 0, sizeof(buffer_mess));
+    }
+    if (temp == -1) //!= len也可以
+    {
+        //如果沒有換行符
+        strncpy(buffer + 9, buffer_mess, len); //全弄过来
+        int length = len + 9;
+
+        for (int j = 0; j < MAX_USERS; j++)
+        {
+            if (used[j] && j != id)
+            {
+                int send_length = 0; //记录已发送的字节数
+                int send_return = 0; // send的返回值
+                while (send_length < length)
+                {
+                    send_return = send(client[j], buffer + send_length, length - send_length, 0); //每次都尝试发送剩下的所有信息
+                    if (send_return == -1)
+                    {
+                        perror("send");
+                        exit(-1);
+                    }
+                    send_length += send_return;
+                }
+            }
+        }
     }
 }
 int main(int argc, char **argv)
